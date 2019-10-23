160c8f34cc1a450f1c195f112e630d27324694d2
zhangtingcen <teeeemoji@163.com>
2019-10-23 15:42:21 +0800

    test: write first test cases

--- __test__/main.test.js
+++ __test__/main.test.js
@@ -9,10 +9,116 @@
  * @author teeeemoji.
  * @since 2019-10-21
  */
-const main = require('../index')
+import {decimalDiv, decimalPlusToString} from '../src'
 
-describe('main', function () {
-  it('should plopfile passed', function (done) {
-    done()
+const {
+  n2d,
+  d2s,
+  n2s,
+  decimalPlus,
+  decimalMinus,
+  decimalTimes
+} = require('../dist/esm/index.esm.js')
+import Decimal from 'decimal.js-light'
+
+describe('Number format', function () {
+  test('digitizer accuracy from 10^20 to 10^-10', function () {
+    expect(
+      n2s('123456789012345678790.12345678901234567890')
+    ).toBe('123456789012345678790.123456789')
+
+    expect(
+      n2s('99999999999999999999.9999999999')
+    ).toBe('99999999999999999999.9999999999')
+
+    expect(
+      n2s(0.99, {maxFixed: 0, minFixed: 0})
+    ).toBe('1')
+
+    expect(
+      n2s('9999999999.999999999', {maxFixed: 2})
+    ).toBe('10000000000.00')
   })
 })
+
+describe('decimalPlus', function () {
+  test('1 + 2 + 3 + 4 === 10', function () {
+    expect(
+      n2s(
+        decimalPlus(1, 2, 3, 4),
+        {minFixed: 0}
+      )
+    ).toBe('10')
+  })
+  test('0.1 + 0.2 === 0.3', function () {
+    expect(
+      n2s(decimalPlus(0.1, 0.2),
+        {minFixed: 0}
+      )
+    ).toBe('0.3')
+  })
+
+})
+
+
+describe('decimalMinus', function () {
+  test('1234567890.1234567890 - 1000000000.000000009 - 200000000.00000008 - 30000000.0000007 === 4567890.123456', function () {
+    expect(
+      n2s(
+        decimalMinus('1234567890.1234567890', '1000000000.000000009', '200000000.00000008', '30000000.0000007'),
+        {minFixed: 0}
+      )
+    ).toBe('4567890.123456')
+  })
+  test('0.4 - 0.1 === 0.3', function () {
+    expect(
+      n2s(
+        decimalMinus(0.4, 0.1),
+        {minFixed: 0}
+      )
+    ).toBe('0.3')
+  })
+})
+
+
+describe('decimalTimes', function () {
+  test('0.0000000001 * 10000000000 * 2 * 3 === 6', function () {
+    expect(
+      n2s(
+        decimalTimes('0.0000000001', '10000000000', 2, 3),
+        {minFixed: 0}
+      )
+    ).toBe('6')
+  })
+  test('Number.MAX_SAFE_INTEGER * 10 === 90071992547409910', function () {
+    expect(
+      n2s(
+        decimalTimes(Number.MAX_SAFE_INTEGER, 10),
+        {minFixed: 0}
+      )
+    ).toBe('90071992547409910')
+  })
+})
+
+
+describe('decimalDiv', function () {
+  test('1000 / 1000 / 1000 / 1000 = 0.000001', function () {
+    expect(
+      n2s(
+        decimalDiv(1000, 1000, 1000, 1000),
+        {minFixed: 0}
+      )
+    ).toBe('0.000001')
+  })
+  test('99999999999999999999.9999999999 / 33333333333333333333.3333333333 / 2 = 1.5', function () {
+    expect(
+      n2s(
+        decimalDiv('99999999999999999999.9999999999', '33333333333333333333.3333333333', 2),
+        {minFixed: 0}
+      )
+    ).toBe('1.5')
+  })
+})
+
+
+
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ export function numberToDecimalObj(number) {
     return number
   }
   if (!number || isNaN(number)) {
-    number = 0
+    throw new Error('please input a Number, or a String of a number')
   }
   return new Decimal(number)
 }
@@ -20,29 +20,59 @@ export const n2d = numberToDecimalObj
  * @param options 设置最大&最小 保留小数位
  * @returns string
  */
-export function decimalToString(number, options = {minFixed: 2, maxFixed: 10}) {
+export function decimalToString(number, {minFixed = 2, maxFixed = 10} = {}) {
   let decimalObj = n2d(number)
-  let decimalFixedStr = decimalObj.toFixed(options.maxFixed)
-  let fixed = decimalFixedStr.replace(/^\d*\./, '').replace(/0*$/, '').length // 这是数字真实小数位数
+  let decimalFixedStr = decimalObj.toFixed(maxFixed)
+  let fixed = decimalFixedStr.replace(/^\d*\.?/, '').replace(/0*$/, '').length // 这是数字真实小数位数
 
-  fixed = Math.max(options.minFixed, fixed)
+  fixed = Math.max(minFixed, fixed)
   return decimalObj.toFixed(fixed)
 }
 
 export const d2s = decimalToString
-// 加
+
+export function numberToString(number, {minFixed = 2, maxFixed = 10} = {}) {
+  return d2s(n2d(number), {minFixed, maxFixed})
+}
+
+export const n2s = numberToString
+
+// 加, 返回 decimal 对象
 export function decimalPlus(num1, ...nums) {
   return nums.reduce((result, num) => result.plus(n2d(num)), n2d(num1))
 }
-// 减
+
+// 加, 返回字符串
+export function decimalPlusToString(num1, ...nums) {
+  return d2s(decimalPlus(num1, ...nums))
+}
+
+// 减, 返回 decimal 对象
 export function decimalMinus(num1, ...nums) {
   return nums.reduce((result, num) => result.minus(n2d(num)), n2d(num1))
 }
-// 乘
+
+// 减, 返回字符串
+export function decimalMinusToString(num1, ...nums) {
+  return d2s(decimalMinus(num1, ...nums))
+}
+
+// 乘, 返回 decimal 对象
 export function decimalTimes(num1, ...nums) {
   return nums.reduce((result, num) => result.times(n2d(num)), n2d(num1))
 }
-// 除
+
+// 乘, 返回字符串
+export function decimalTimesToString(num1, ...nums) {
+  return d2s(decimalTimes(num1, ...nums))
+}
+
+// 除, 返回 decimal 对象
 export function decimalDiv(num1, ...nums) {
   return nums.reduce((result, num) => result.div(n2d(num)), n2d(num1))
+}
+
+// 除, 返回字符串
+export function decimalDivToString(num1, ...nums) {
+  return d2s(decimalDiv(num1, ...nums))
 }
